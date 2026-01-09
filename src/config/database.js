6827677c502efa66163d2cbe3954267a08b07c4e
Zahara Putri <email_github_kamu@example.com>
2026-01-09 23:38:52 +0700

    Connect to Railway MySQL

--- src/config/database.js
+++ src/config/database.js
@@ -1,10 +1,11 @@
 const mysql = require('mysql2');
 
 const db = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'category_db'
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+  port: process.env.MYSQLPORT
 });
 
 db.connect((err) => {
